b764c60915b6e1d166290ca7dcd9cf76d1bd71a8
afreen08 <71078194+afreen08@users.noreply.github.com>
2020-09-11 16:39:46 +0530

    Add files via upload

---
+++ assignmentday3.py
@@ -0,0 +1,22 @@
+
+#prime
+for i in range(1,200+1):
+    count=0
+    for k in range(1,i+1):
+        if(i%k==0):
+            count+=1
+    if(count==2):
+           print(i)
+
+
+
+
+
+#condition
+n=int(input("enter the number="))
+if(n==1000):
+    print("safe to land")
+elif n<=4500:
+    print("bringit to 1000ft")
+else:
+    print("turn around")
